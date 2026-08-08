77a51030ce0261995e273408e993062e922d5aa4
Tuyen <tuyenctbk@gmail.com>
2026-08-08 12:30:18 +0700

    Fix AAPT2 string compilation failure by stripping duplicate backslashes in generate_translations.py

--- generate_translations.py
+++ generate_translations.py
@@ -313,7 +313,8 @@ for lang in langs:
             text = val_dict.get(lang, val_dict.get("default", default_text))
         else:
             text = default_text
-        text_escaped = text.replace("&", "&amp;").replace("'", "\\'")
+        clean_text = text.replace("\\", "")
+        text_escaped = clean_text.replace("&", "&amp;").replace("'", "\\'")
         lines.append(f'    <string name="{key}">{text_escaped}</string>\n')
     lines.append("</resources>\n")
 
