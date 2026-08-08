c6392ef35b2c5fa967e010aa36462506761795e1
Tuyen <tuyenctbk@gmail.com>
2026-08-08 16:05:14 +0700

    Bump versionCode to 15 (v2.5) for Google Play Health policy compliance release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
     applicationId = "com.soloprono.swaift"
     minSdk = 24
     targetSdk = 36
-    versionCode = 14
-    versionName = "2.4"
+    versionCode = 15
+    versionName = "2.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
