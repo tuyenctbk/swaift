c967bbe5a469a59be329b77716ed3a842f1c3822
Tuyen <tuyenctbk@gmail.com>
2026-08-08 14:04:46 +0700

    chore: bump app version to 2.3 (code 13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
     applicationId = "com.soloprono.swaift"
     minSdk = 24
     targetSdk = 36
-    versionCode = 12
-    versionName = "2.2"
+    versionCode = 13
+    versionName = "2.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
